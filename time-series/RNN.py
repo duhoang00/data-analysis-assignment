3ffda77d294e68728c210b28bd47b95b360d6254
duc <ductmh2409@gmai.com>
2021-08-18 09:49:43 +0700

    a

--- time-series/RNN.py
+++ time-series/RNN.py
@@ -7,28 +7,23 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from create_data import create_data
-
 #%% Load data
 data = pd.read_csv('./data/airline-passengers.csv', usecols=[1])
 plt.plot(data)
 plt.show()
-
 #%% Normalize data
 sc = MinMaxScaler(feature_range=(0,1))
 data = sc.fit_transform(data)
-
 #%% Split data into train, test
 train_ = int(len(data)*0.7)
 test_ = len(data) - train_
 train = data[0:train_,:]
 test = data[train_:len(data),:]
 print(len(train), len(test))
-
 #%% Reshape data into X = t, Y = t + 1
 look_back = 1
 trainX, trainY = create_data(train, look_back)
 testX, testY = create_data(test, look_back)
-
 #%% reshape input to [samples, time_steps, features]
 trainX = np.reshape(trainX, (trainX.shape[0],1,trainX.shape[1]))
 testX = np.reshape(testX,(testX.shape[0],1,testX.shape[1]))
@@ -48,6 +43,8 @@ y_pred = sc.inverse_transform(y_pred)
 testY = testY.reshape(-1,1)
 testY = sc.inverse_transform(testY)
 #%% plot baseline and prediction
-plt.plot(testY)
-plt.plot(y_pred)
+plt.plot(testY, label='Real number of passengers')
+plt.plot(y_pred, label ='Predicted number of passengers')
+plt.ylabel('# Passengers')
+plt.legend()
 plt.show()
