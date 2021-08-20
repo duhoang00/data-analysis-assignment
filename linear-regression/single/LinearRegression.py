6968152ca06e1a796d30f0da4d80e003c9dd83ed
hoangminhdu2000@gmail.com <hoangminhdu2000@gmail.com>
2021-08-20 20:43:36 +0700

    update Single Linear Regression

--- linear-regression/single/LinearRegression.py
+++ linear-regression/single/LinearRegression.py
@@ -17,60 +17,6 @@ df.drop_duplicates(inplace=True)
 df['smoker'] = df['smoker'].replace({"yes": 1, "no": 0})
 df['sex'] = df['sex'].replace({"male": 1, "female": 0})
 
-# %% - Check age outliers
-orange_square = dict(markerfacecolor='orange', marker='s')
-plt.boxplot(df['age'], vert=False, notch=True, flierprops=orange_square)
-plt.xlabel("Age")
-plt.title("Age Outliers")
-plt.show()
-
-# %% - Check BMI outliers
-orange_square = dict(markerfacecolor='orange', marker='s')
-plt.boxplot(df['bmi'], vert=False, notch=True, flierprops=orange_square)
-plt.xlabel("BMI")
-plt.title("BMI Outliers")
-plt.show()
-
-# %% - Check charges outliers
-orange_square = dict(markerfacecolor='orange', marker='s')
-plt.boxplot(df['charges'], vert=False, notch=True, flierprops=orange_square)
-plt.xlabel("Charges")
-plt.title("Charges Outliers")
-plt.show()
-
-# %% - Check Age and BMI outliers - DU
-age_bmi = sns.scatterplot(x='age', y='bmi', hue='bmi', data=df)
-plt.title('Age & BMI outliers')
-plt.xlabel("Age")
-plt.ylabel("BMI")
-plt.show()
-
-# %% - Age and Charges by BMI outliers - KA
-age_charges_bmi = sns.scatterplot(x='age', y='charges', hue='bmi', data=df)
-plt.title("Age vs Charges by BMI")
-plt.legend()
-plt.xlabel("Age")
-plt.ylabel("Charges")
-plt.show()
-
-
-# %% - Age and Charges by Sex outliers - DUC
-age_charges_sex = sns.scatterplot(
-    x='age', y='charges', hue='sex', style='sex', data=df)
-age_charges_sex.set_title("Age vs Charges by Sex")
-plt.xlabel("Age (Male - 1, No - 0)")
-plt.ylabel("Charges")
-plt.legend()
-plt.show()
-
-# %% Age and Charges by Smoker outliers - DAT
-age_charges_smoker = sns.scatterplot(
-    x='age', y='charges', hue='smoker', data=df)
-age_charges_smoker.set_title("Age vs Charges by Smoker")
-plt.xlabel("Smoker (Yes - 1, No - 0)")
-plt.ylabel("Charges")
-plt.show()
-
 # %% - Check relationship
 df.corr()
 # smoker has 78.7% impact to charges
@@ -86,7 +32,7 @@ plt.ylabel("Charges")
 plt.show()
 
 # %% - Create model
-x = df[['age', 'bmi', 'smoker']]
+x = df[['smoker']]
 y = df['charges']
 X_train, X_test, y_train, y_test = train_test_split(
     x, y, test_size=0.3, random_state=42)
@@ -110,7 +56,6 @@ plt.ylabel("Predicted Charges")
 plt.show()
 
 # %% - Predict future values
-future_x = [[24, 24.22, 1], [22, 20.98, 0], [21, 24.19, 0], [21, 21.6, 0]]
+future_x = [[1]]
 future_y = model.predict(future_x)
-
-# %%
+print(future_y)
