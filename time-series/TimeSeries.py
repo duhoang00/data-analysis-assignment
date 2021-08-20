5412d4352eb3c026bf2ead294c60b13eacc6bdcc
hoangminhdu2000@gmail.com <hoangminhdu2000@gmail.com>
2021-08-20 21:13:44 +0700

    update Time Series

--- time-series/TimeSeries.py
+++ time-series/TimeSeries.py
@@ -1,4 +1,4 @@
-#%% Import libraries
+# %% Import libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 from statsmodels.tsa.stattools import adfuller
@@ -6,46 +6,47 @@ from statsmodels.graphics.tsaplots import plot_pacf
 from statsmodels.tsa.arima_model import ARIMA
 from pmdarima import auto_arima
 
-#%% Import dataset
+# %% Import dataset
 data = pd.read_csv('./data/Sunspots.csv')
 print(data)
 
-#%% Data overview
+# %% Data overview
 plt.plot(data['Monthly Mean Total Sunspot Number'])
 
-#%% Check stationary
+# %% Check stationary
 stationary_test = adfuller(data['Monthly Mean Total Sunspot Number'])
 print(stationary_test)
 
-#%% PACF
+# %% PACF
 pacf = plot_pacf(data['Monthly Mean Total Sunspot Number'])
 
-#%% AR model
-AR = ARIMA(data['Monthly Mean Total Sunspot Number'], order = (1,0,0)).fit()
+# %% AR model
+AR = ARIMA(data['Monthly Mean Total Sunspot Number'], order=(1, 0, 0)).fit()
 print(AR.summary())
 
-#%% AR visuallize
+# %% AR visuallize
 pred = AR.predict()
 plt.plot(data['Monthly Mean Total Sunspot Number'])
-plt.plot(pred, color ='red')
+plt.plot(pred, color='red')
 
-#%% ARMA model
-ARMA = ARIMA(data['Monthly Mean Total Sunspot Number'], order = (1,0,1)).fit()
+# %% ARMA model
+ARMA = ARIMA(data['Monthly Mean Total Sunspot Number'], order=(1, 0, 1)).fit()
 print(ARMA.summary())
 
-#%% ARMA visuallize
+# %% ARMA visuallize
 predict = ARMA.predict()
 plt.plot(data['Monthly Mean Total Sunspot Number'])
-plt.plot(predict, color ='red')
+plt.plot(predict, color='red')
 
-#%% Auto calculate pdq
-pdq = auto_arima(data['Monthly Mean Total Sunspot Number'], trace= True, suppress_warnings=True)
+# %% Auto calculate pdq
+pdq = auto_arima(data['Monthly Mean Total Sunspot Number'],
+                 trace=True, suppress_warnings=True)
 
-#%% ARIMA model
-ARIMA = ARIMA(data['Monthly Mean Total Sunspot Number'], order = (1,0,2)).fit()
+# %% ARIMA model
+ARIMA = ARIMA(data['Monthly Mean Total Sunspot Number'], order=(1, 0, 2)).fit()
 print(ARIMA.summary())
 
-#%% ARIMA visuallize
+# %% ARIMA visuallize
 pre = ARIMA.predict()
 plt.plot(data['Monthly Mean Total Sunspot Number'])
-plt.plot(pre, color ='red')
+plt.plot(pre, color='red')
