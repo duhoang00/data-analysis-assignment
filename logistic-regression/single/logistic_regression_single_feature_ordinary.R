e25891419149c2af53e60e9a49d9eec321f73db3
ThanhDat <datnt1999py>
2021-08-20 21:51:04 +0700

    logistic regression update

--- logistic-regression/single/logistic_regression_single_feature_ordinary.R
+++ logistic-regression/single/logistic_regression_single_feature_ordinary.R
@@ -1,74 +1,48 @@
-#Load Library
-library(MASS)
-library(foreign)
-library(ggplot2)
-library(reshape2)
-library(Hmisc)
-#Read Data
-dat <- read.dta("https://stats.idre.ucla.edu/stat/data/ologit.dta")
-#View Data
-head(dat)
-#Descriptive statistics of these variables
-lapply(dat[, c("apply", "pared", "public")], table)
-ftable(xtabs(~ public + apply + pared, data = dat))
-summary(dat$gpa)
-sd(dat$gpa)
-#Plot all of the marginal relationships
-ggplot(dat, aes(x = apply, y = gpa)) +
-  geom_boxplot(size = .75) +
-  geom_jitter(alpha = .5) +
-  facet_grid(pared ~ public, margins = TRUE) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
-#Fit ordered logit model and store results 'm'
-m <- polr(apply ~ pared + public + gpa, data = dat, Hess=TRUE)
-#View a summary of the model
-summary(m)
-#Store table
-(ctable <- coef(summary(m)))
-#Calculate and store p values
-p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
-#Combined table
-(ctable <- cbind(ctable, "p value" = p))
-#Default method gives profiled CIs
-(ci <- confint(m))
-#CIs assuming normality
-confint.default(m)
-#Odds ratios
-exp(coef(m))
-#OR and CI
-exp(cbind(OR = coef(m), ci))
-#The summary function
-sf <- function(y) {
-  c('Y>=1' = qlogis(mean(y >= 1)),
-    'Y>=2' = qlogis(mean(y >= 2)),
-    'Y>=3' = qlogis(mean(y >= 3)))
-}
+# - Import Data
+library(dplyr)
+library(scatterplot)
+library(caTools)
+library(ROCR)
 
-(s <- with(dat, summary(as.numeric(apply) ~ pared + public + gpa, fun=sf)))
-#
-glm(I(as.numeric(apply) >= 2) ~ pared, family="binomial", data = dat)
-#
-glm(I(as.numeric(apply) >= 3) ~ pared, family="binomial", data = dat)
-#Print
-s[, 4] <- s[, 4] - s[, 3]
-s[, 3] <- s[, 3] - s[, 3]
-s 
-plot(s, which=1:3, pch=1:3, xlab='logit', main=' ', xlim=range(s[,3:4]))
-#
-newdat <- data.frame(
-  pared = rep(0:1, 200),
-  public = rep(0:1, each = 200),
-  gpa = rep(seq(from = 1.9, to = 4, length.out = 100), 4))
+# - Load Data
+ads <- read.csv(file.choose())
+head(ads)
+# -Replace NA values in dataset
+ads[is.na(ads)]=0
+head(ads)
 
-newdat <- cbind(newdat, predict(m, newdat, type = "probs"))
-#show first few rows
-head(newdat)
-#
-lnewdat <- melt(newdat, id.vars = c("pared", "public", "gpa"),
-  variable.name = "Level", value.name="Probability")
-#view first few rows
-head(lnewdat)
-#
-ggplot(lnewdat, aes(x = gpa, y = Probability, colour = Level)) +
-  geom_line() + facet_grid(pared ~ public, labeller="label_both")
+colnames(ads)[:-1] <- "Clicked.on.Ad"
+head(ads)
+# Split train & test
+set.seed(100)
+split = sample.split(ads$Clicked.on.Ad, SplitRatio = 0.65)
+train = subset(ads,split==TRUE)
+test = subset(ads,split==FALSE)
+head(train)
+# - CreateModel
+model <- glm(Clicked.on.Ad ~ Age, data = train,  family = binomial)
+summary(model)
 
+# - GetResult
+coef(model) %>% 
+  exp() %>% 
+  round(digits = 6)
+
+# - Create Predict base on train data
+y_pred <-predict(model,train,type="response")
+summary(y_pred)
+
+# - Create Predict base on test data
+y_test <-predict(model,test,type="response")
+summary(y_test)
+
+# - Confusion Matrxi
+table(test$Clicked.on.Ad,y_test > 0.5)
+
+# - Visualize
+ROCRpred = prediction(y_pred,train$Clicked.on.Ad)
+ROCRperf = performance(ROCRpred,"tpr","fpr")
+plot(ROCRperf,colorize=TRUE)
+
+# - Caculate Accuracy Score
+as.numeric(performance(ROCRpred,"auc")@y.values)
--- logistic-regression/single/logistic_regression_single_feature_ordinary.py
+++ logistic-regression/single/logistic_regression_single_feature_ordinary.py
@@ -1,87 +1,72 @@
-#%%Import library
-import pandas as pd
-import numpy as np
+#%% - Import Library
+import numpy as np 
+import pandas as pd 
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-#%%Congigs
-plt.rcParams['figure.figsize'] = (10,8)
-plt.rcParams['figure.dpi'] = 200
+#%% - Load Data
+ads = pd.read_csv("./advertising.csv")
+ads.head(10)
 
-#%%Load data
-df = pd.read_csv('./data/Insurance.csv')
+#%% - Validate data
+ads.isnull().sum()
+ads.duplicated().sum()
+ads.drop_duplicates(inplace=True)
+
+#%% - Exploratory data analysis
+ads.info()
+
+#%% 
+ads.describe()
 
 #%%
-plt.scatter(df.age, df['bought_insurance'], color='green', marker='o')
-plt.show()
+sns.pairplot(ads,hue='Clicked on Ad')
 
-#%%Split train/test dataset
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(df[['age']], df.bought_insurance, test_size=0.1)
+#%% - Split train/test dataset
+x = ads[[ 'Age']]
+y = ads['Clicked on Ad']
 
-#%%Create model
-from sklearn.linear_model import LogisticRegression
-model = LogisticRegression().fit(X_train, y_train)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
+#%% -Create Model
+model = LogisticRegression().fit( X_train, y_train )
 
-#%%Get results
+#%% - Get result
 intercept = model.intercept_
 coefs = model.coef_
-score = model.score(X_train,y_train)
-prob_matrix = model.predict_proba(X_train)
-print(intercept)
+score = model.score( X_train, y_train )
+prob_matrix = model.predict_proba( X_train )
+
 print(coefs)
+print(intercept)
+print(score)
 
-#%%
-from sklearn.metrics import classification_report, confusion_matrix
-y_predict = model.predict(X_train)
-print(classification_report(y_train, y_predict))
-cm = confusion_matrix(y_train,y_predict)
+#%% - Show Classification Report and Accuracy Score
+y_pred = model.predict(X_test)
+print("Classification report:\n",classification_report(y_test, y_pred))
+print("Accuracy score:",accuracy_score(y_test,y_pred))
 
-#%%
-plt.scatter(X_train, y_train, marker='x',label='Actual')
-plt.scatter(X_train, y_predict, color='red',marker='o', label='Predict')
-plt.legend()
-plt.show()
-
-#%%
+#%% - Visualize Confusion Matrix
+cm = confusion_matrix( y_test, y_pred )
 fig, ax = plt.subplots()
-ax.imshow(cm)
-ax.xaxis.set(ticks=(0,1), ticklabels=('Predict 0s', 'Predict 1s'))
-ax.yaxis.set(ticks=(0,1), ticklabels=('Actual 0s', 'Actual 1s'))
+ax.imshow( cm )
+ax.xaxis.set(ticks=(0, 1), ticklabels=('Predicted 0s', 'Predicted 1s'))
+ax.xaxis.set(ticks=(0, 1), ticklabels=('Actual 0s', 'Actual 1s'))
 for i in range(2):
-  for j in range(2):
-    ax.text(j, i, cm[i, j], ha ='center', va ='center', color='red', fontsize='26')
-
+    for j in range(2):
+            ax.text(j, i, cm[i, j], ha='center', va='center', color='white',fontsize=26)
 plt.show()
 
-#%%Predict
-pred_values = model.predict(X_test)
-pred_score = model.score(X_test,y_test)
-pred_prob_matrix = model.predict_proba(X_test)
 
-#%%
-
-import math
-def sigmoid(x):
-  return 1 / (1+math.exp(-x))
-
-#%%Define prediction function via Sigmoid
-def prediction_func(age, inter, coef):
-  x = inter + coef * age
-  return sigmoid(x)
-
-#%%Draw Sigmoid Plot
-plt.scatter(X_train, y_train, color='r',marker='o')
-X_test = np.linspace(10,75,25)
-sigs = []
-for item in X_test:
-  sigs.append(prediction_func(item, intercept[0], coefs[0][0]))
-plt.plot(X_test, sigs, color = 'green')
-plt.scatter(X_test, y_test, color='b', s=150, label='Actual')
-plt.scatter(X_test, pred_values,color='orange', label='Predict')
-plt.legend(loc='center right')
+# %%
+y_pred_proba = model.predict_proba(X_test)[::,1]
+fpr, tpr, _ = metrics.roc_curve(y_test,  y_pred_proba)
+auc = metrics.roc_auc_score(y_test, y_pred_proba)
+plt.plot(fpr,tpr,label="predict value, auc="+str(auc))
+plt.legend(loc=4)
 plt.show()
 
-#%%Predict for future values
-pred_prob = prediction_func(56, intercept[0],coefs[0][0])
-print(pred_prob)
