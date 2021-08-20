756fb145bbe947b49217c093ab7c52cad367419d
hoangminhdu2000@gmail.com <hoangminhdu2000@gmail.com>
2021-08-20 21:31:06 +0700

    update SVM

--- machine-learning/support-vector-machine/SVM_in_R.R
+++ machine-learning/support-vector-machine/SVM_in_R.R
@@ -2,10 +2,12 @@
 library(caTools)
 library(e1071)
 library(readr)
+
 #Load Dataset
-social <- read_csv(".machine-learning/support-vector-machine/social.csv")
+social <- read_csv("./machine-learning/support-vector-machine/social.csv")
 social = social[3:5]
 social$Purchased = factor(social$Purchased, levels = c(0, 1))
+
 #Splitting the dataset
 set.seed(123)
 split = sample.split(social$Purchased, SplitRatio = 0.75)
@@ -14,13 +16,17 @@ training_set = subset(social, split == TRUE)
 test_set = subset(social, split == FALSE)
 training_set[-3] = scale(training_set[-3])
 test_set[-3] = scale(test_set[-3])
+
 #Fitting SVM to the training set
 classifier = svm(formula = Purchased ~ ., data = training_set, type = 'C-classification', kernel = 'linear')
+
 #Summary
 summary(classifier)
+
 #Predicting the Test set results
 y_pred = predict(classifier, newdata = test_set[-3])
 cm = table(test_set[, 3], y_pred)
+
 #Visualizing the Training set results
 set = training_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
@@ -40,6 +46,7 @@ contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'coral1', 'aquamarine'))
   
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
 #Visualizing the Test set results
 set = test_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
@@ -58,4 +65,3 @@ contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'coral1', 'aquamarine'))
   
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
-
